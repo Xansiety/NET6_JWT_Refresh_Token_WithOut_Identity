95375f6a9f907b1b5b33d6fe7559850e3212bf49
xansiety <ferando543@outlook.com>
2022-09-03 15:13:58 -0500

    setup Serilog.AspNetCore as LoggerDefault

---
+++ Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace NET6_WEB_API_TEMPLATE_JWT.Extensions
+{
+    public class ApplicationServiceExtensions
+    {
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
 using Microsoft.EntityFrameworkCore;
-using NET6_WEB_API_TEMPLATE_JWT;
+using NET6_WEB_API_TEMPLATE_JWT; 
+using Serilog; //Serilog.AspNetCore Package
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// SeriLog->Configuración obtenida desde AppSettings
+var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+
+//DB->CONNECTION
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+//SERILOG ->LOGGER
+builder.Logging.AddSerilog(logger: logger);
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -16,8 +28,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
 //{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+app.UseSwagger();
+app.UseSwaggerUI();
 //}
 
 app.UseHttpsRedirection();
